fdcdb8cde1eb62cdba4747678ac3444c8c68fed7
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-23 13:46:56 +0100

    dla kacpra 1

--- js/app.js
+++ js/app.js
@@ -1,24 +1,14 @@
-//import of songs array and DOM cache
-
-import {
-    songsArr
-  } from '/js/songs.js'
   import {
-    // coverEl,
-    songsCounterEl,
     playButtonLeftEl,
-    rightPanel,
-    progressBarPercentage,
-    // progressBar
   } from '/js/dom.js'
 
-const selectedPlaylistElementIndex = null;
-const currentlyPlayingSongIndex = null;
 const song = new Audio();
-// song.src = '../music/morrowind.mp3'
+
+// song.src = '/musicplayer3/music/morrowind.mp3'
+
 
 const playIt = () => {
-    song.src = '../music/morrowind.mp3'
+    song.src = '/musicplayer3/music/morrowind.mp3'
     if (song.paused) {
         console.log("1")
         song.play();
