817b1bd15063b195200eb65127d8eb263973b028
Mikolaj Walanus <mikolaj@walanus.com>
2020-11-23 13:34:47 +0100

    part 2

--- js/app.js
+++ js/app.js
@@ -1,105 +1,82 @@
 //import of songs array and DOM cache
+
 import {
-  songsArr
-} from '/js/songs.js'
-import {
-  // coverEl,
-  songsCounterEl,
-  playButtonLeftEl,
-  rightPanel,
-  progressBarPercentage,
-  // progressBar
-} from '/js/dom.js'
+    songsArr
+  } from '/js/songs.js'
+  import {
+    // coverEl,
+    songsCounterEl,
+    playButtonLeftEl,
+    rightPanel,
+    progressBarPercentage,
+    // progressBar
+  } from '/js/dom.js'
 
-// Left panel
+const selectedPlaylistElementIndex = null;
+const currentlyPlayingSongIndex = null;
+const song = new Audio();
+// song.src = '../music/morrowind.mp3'
 
-songsCounterEl.innerHTML = `${songsArr.length} songs`;
-
-// Song module constructor
-
-let moduleNumberReturned;
-let modulesArr = [];
-
-function Module(moduleId) {
-
-  this.playModuleEl = document.createElement("div");
-  this.audioEl = document.createElement("audio");
-  this.timerInterval;
-  this.moduleNumber = moduleId;
-
-}
-
-Module.prototype.playModuleRender = function (songId) {
-
-  let playButtonRight = document.createElement("div");
-  let titleAreaEl = document.createElement("div");
-  let titleContentEl = document.createElement("div");
-  let titleAlbumEl = document.createElement("p");
-  let titleSongEl = document.createElement("p");
-  let songDurationEl = document.createElement("div");
-
-  this.playModuleEl.classList.add("play__module__content");
-  this.playModuleEl.setAttribute("id", `playmodule${songId}`);
-  playButtonRight.classList.add("play__button__right");
-  playButtonRight.setAttribute("id", `playbuttonright${songId}`)
-  this.audioEl.setAttribute("src", `${songsArr[songId]["url"]}`)
-  titleAreaEl.classList.add("title__area");
-  titleContentEl.classList.add("title__content");
-  titleAlbumEl.classList.add("title__album");
-  titleSongEl.classList.add("title__song");
-  songDurationEl.classList.add("song__duration");
-
-  rightPanel.appendChild(this.playModuleEl);
-  this.playModuleEl.appendChild(playButtonRight);
-  playButtonRight.innerHTML = "&#9658;";
-  playButtonRight.appendChild(this.audioEl);
-
-  this.playModuleEl.appendChild(titleAreaEl);
-  titleAreaEl.appendChild(titleContentEl);
-  titleContentEl.appendChild(titleAlbumEl);
-  titleContentEl.appendChild(titleSongEl);
-  this.playModuleEl.appendChild(songDurationEl);
-
-  titleAlbumEl.innerHTML = `${songsArr[songId]["title"]}`
-  titleSongEl.innerHTML = `${songsArr[songId]["artist"]}`
-  songDurationEl.innerHTML = `${songsArr[songId]["time"]}`
-};
-
-Module.prototype.play = function (audioArg, timerArg, moduleArg) {
-
-  this.playModuleEl.addEventListener("click",
-    function () {
-      console.log(moduleArg);
-      if (audioArg.paused) {
-        for (let elem of modulesArr) {
-          elem.audioEl.pause();
-        }
-        audioArg.play();
-        timerArg = setInterval(function () {
-          progressBarPercentage.style.width = `${audioArg.currentTime / audioArg.duration * 100}%`;
-        }, 50);
+const playIt = () => {
+    song.src = '../music/morrowind.mp3'
+    if (song.paused) {
+        console.log("1")
+        song.play();
       } else {
-        audioArg.pause();
-        clearInterval(timerArg);
-      }
-      moduleNumberReturned = moduleArg;
-      return moduleNumberReturned;
+        song.pause(); 
+        console.log("2")
+      }      
     }
-  )
-}
 
-// Playlist (Song modules) generator
+playButtonLeftEl.addEventListener("click", playIt);
 
-for (let i = 0; i < songsArr.length; i++) {
-  let module = new Module(i);
-  modulesArr.push(module);
-  module.playModuleRender(i);
-  module.play(module.audioEl, module.timerInterval, module.moduleNumber);
-  // console.log(modulesArr[i]);
-}
 
-// console.log(modulesArr[moduleNumberReturned].audioEl)
-playButtonLeftEl.onclick = function () {
-console.log("bzik")
-  modulesArr[moduleNumberReturned].play(modulesArr[moduleNumberReturned].audioEl, modulesArr[moduleNumberReturned].timerInterval, modulesArr[moduleNumberReturned].moduleNumber)
-}
+
+
+
+// Module.prototype.play = function (audioArg, timerArg, moduleArg) {
+
+//   this.playModuleEl.addEventListener("click",
+//     function () {
+//       console.log(moduleArg);
+//       if (audioArg.paused) {
+//         for (let elem of modulesArr) {
+//           elem.audioEl.pause();
+//         }
+//         audioArg.play();
+//         timerArg = setInterval(function () {
+//           progressBarPercentage.style.width = `${audioArg.currentTime / audioArg.duration * 100}%`;
+//         }, 50);
+//       } else {
+//         audioArg.pause();
+//         clearInterval(timerArg);
+//       }
+//       moduleNumberReturned = moduleArg;
+//       return moduleNumberReturned;
+//     }
+//   )
+// }
+
+
+
+
+
+
+
+// const createModule = _ => {
+
+//     playModule.innerHTML = `
+//   <div class="play__module__content">
+//     <div class="play__button__right">
+//         &#9658;
+//         <audio src="${song["url"]}"></audio>
+//     </div>
+//     <div class="title__area">
+//       <div class="title__content">
+//         <p class="title__album">${song["title"]}</p>
+//         <p class="title__song">${song["artist"]}</p>
+//       </div>
+//     </div>
+//     <div class="song__duration">${song["time"]}</div>
+//   </div>`
+//   }
